e5b614080d2fdb6f50a754ff9a6a71852e614f3b
bigxbishop-lang <bigxbishop@gmail.com>
2026-05-10 22:38:41 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { Routes, Route } from "react-router-dom";
-import Nav from "./components/nav";
-import { Toaster } from "./components/ui/toaster";
-import Home from "./pages/home";
-import Apply from "./pages/apply";
-import Customize from "./pages/customize";
-import Race from "./pages/race";
+import { Switch, Route } from 'wouter';
+import Home from './pages/Home';
+import ShellBlitz from './pages/ShellBlitz';
+import Race from './pages/Race';
+import Customize from './pages/Customize';
+import Gallery from './pages/Gallery';
+import NotFound from './pages/NotFound';
 
-export default function App() {
+function App() {
   return (
-    <>
-      <Nav />
-      <Toaster />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/apply" element={<Apply />} />
-        <Route path="/customize" element={<Customize />} />
-        <Route path="/race" element={<Race />} />
-      </Routes>
-    </>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/shell-blitz" component={ShellBlitz} />
+      <Route path="/race" component={Race} />
+      <Route path="/customize" component={Customize} />
+      <Route path="/gallery" component={Gallery} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
+
+export default App;
