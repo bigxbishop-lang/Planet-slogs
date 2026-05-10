74712f166637326dd5b74a768edb92a1481445c4
bigxbishop-lang <bigxbishop@gmail.com>
2026-05-10 22:32:27 +0100

    Create assets.ts

---
+++ src/lib/assets.ts
@@ -0,0 +1,9 @@
+export const ASSETS = {
+  shellblitz: 'https://lbcnvlvdrjsnpvxxqzzp.supabase.co/storage/v1/object/public/Planetslog/shellblitz.png',
+  chestClosed: 'https://lbcnvlvdrjsnpvxxqzzp.supabase.co/storage/v1/object/public/Planetslog/slogchest-closed.png',
+  chestOpen: 'https://lbcnvlvdrjsnpvxxqzzp.supabase.co/storage/v1/object/public/Planetslog/slogchest-open.png',
+  goldenShell: 'https://lbcnvlvdrjsnpvxxqzzp.supabase.co/storage/v1/object/public/Planetslog/golden-shell.png',
+  dailyShells: 'https://lbcnvlvdrjsnpvxxqzzp.supabase.co/storage/v1/object/public/Planetslog/daily-shells.png',
+  slograce: 'https://lbcnvlvdrjsnpvxxqzzp.supabase.co/storage/v1/object/public/Planetslog/slograce.png',
+  customise: 'https://lbcnvlvdrjsnpvxxqzzp.supabase.co/storage/v1/object/public/Planetslog/customise.png',
+};
